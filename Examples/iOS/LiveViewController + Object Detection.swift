173e8a47f50e172841b468eba29efcefe5e95a94
Subeen Park <psb021414@gmail.com>
2021-06-09 11:42:49 +0900

    remove unused files

--- Examples/iOS/LiveViewController + Object Detection.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LiveViewController + Object Detection.swift
-//  Example iOS
-//
-//  Created by Subeen Park on 2021/06/04.
-//  Copyright © 2021 Shogo Endo. All rights reserved.
-//
-
-import Foundation
